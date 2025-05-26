cfcfeac0d16b1882d1287fb475dddd9698965e4c
r Name <rkruhankhan1913@gmail.com>
2025-05-26 20:05:58 +0600

    dotenv and gitignor file created,building api to store and collect data to database

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const app = app.express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
 const port=process.env.PORT || 5000 ;
 
 
@@ -11,6 +13,56 @@ app.use(cors());
 
 
 
+
+
+const uri = `mongodb+srv://${process.env.user_Name}:${process.env.pass_Db}@cluster0.jwnb6pt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+     const equipmentCollection = client.db("equipmentDB").collection("equipments");
+    app.post('/equipments',async(req,res)=>{
+        const newEquipmet = req.body;
+        const result = await equipmentCollection.insertOne(newEquipmet);
+        res.send(result);
+     
+    })
+
+    app.get('/equipments', async (req, res) => {
+      
+      const cursor = equipmentCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get('/equipments/:email', async (req, res) => {
+      const email=req.params.email;
+      const query = { userEmail: email };
+      const equipment = await equipmentCollection.find(query).toArray();
+      if (equipment) {
+        res.send(equipment);
+      } else {
+        res.status(404).send({ message: "Equipment not found" });
+      }})
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
