a99506d77da2ecf61e48cf4edd2af013de4edd04
r Name <rkruhankhan1913@gmail.com>
2025-05-30 06:19:43 +0600

    final cheke done, removed unUsed lines ,vercel.jsone file created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -29,15 +29,16 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const equipmentCollection = client.db("equipmentDB").collection("equipments");
-    // get all equipments
+
     app.get('/equipments', async (req, res) => {
-      const cursor = equipmentCollection.find().limit(6);
+      const cursor = equipmentCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
+
 
     app.get('/equipments/email/:email', async (req, res) => {
       const email = req.params.email;
@@ -58,11 +59,6 @@ async function run() {
       res.send(equipment);
 
     });
-    // get equipment by id
-
-
-
-
 
 
     app.post('/equipments', async (req, res) => {
@@ -71,12 +67,6 @@ async function run() {
       res.send(result);
 
     })
-
-
-
-
-
-    //   get equipmet by id
 
 
     app.delete('/equipments/:id', async (req, res) => {
@@ -117,21 +107,14 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-
-
-
-
-
-
 
 app.get('/', (req, res) => {
   res.send('server is rinning')
